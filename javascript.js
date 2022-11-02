9c41703c7d123a02cf9dc4ec2b08deb4a32e71b2
Gnesiuc <gnesiuc11998@gmail.com>
2022-11-02 20:35:13 +0200

    Prohodili massivi, obiecti i functii

---
+++ javascript.js
@@ -0,0 +1,93 @@
+// const arr = [];
+// const arr1 = [1, 2, 3,4, 'Hi my friend'];
+// console.log(arr);
+// console.log(arr1.length); 
+
+
+// const user = prompt('What is your name?');
+// const user1 = prompt('How do you have ear?');
+
+// const userArray = [];
+// const userArray1 = ['ion'];
+
+
+// userArray.push(userArray1);
+// userArray.push(user);
+// userArray.push(user1);
+// console.log(userArray);
+
+// let obj = {
+//     name:'Vasilie',
+//     age:'26',
+//     gen:'M',
+// }
+
+// console.log(obj);
+// console.log(obj.name);
+// console.log(obj.age);
+
+
+// let obj_1 = {
+//     name:'Nina',
+//     age:'39',
+//     gen:'F',
+//     nameChild:{
+//         name:'Viorel',
+//         age:'5',
+//         gen:'M'
+//     }
+// }
+// console.log(typeof obj_1.nameChild.age)
+
+
+
+// const user = [
+//     {name:'Alex'},
+//     {name:'Ion'}
+
+// ]
+
+
+// user.forEach(el => {
+//     console.log(el.name);
+// })
+
+
+const array = [
+    'Odin',
+    'Dva',
+    'Tri',
+    'Cetire',
+    'Piati',
+    'Sesti',
+
+];
+
+// function name() {
+//     console.log(array);
+// }
+
+// name()
+
+// function name1(params, inde) {
+//     let nul = params +inde;
+//     console.log(nul);
+// }
+
+// name1('salut');
+// name1(array);
+// name1(45,45)
+
+
+
+
+// array.forEach(el => {
+
+//     // console.log(el.includes('Sesti'));
+
+//     if (el.includes('Piati')) {
+//         console.log(el);
+//     }else{
+//         console.log('Netu');
+//     }
+// })
