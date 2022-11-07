d4837d2e287792682f485e3281e86e477392b8a6
Gnesiuc <gnesiuc11998@gmail.com>
2022-11-07 20:32:50 +0200

    Razobralsea kak Js mojet rabotati s html

---
+++ domjavascript.js
@@ -0,0 +1,29 @@
+
+
+
+
+const tag_h1 = document.querySelector(".title");
+tag_h1.innerHTML = "Salut";
+tag_h1.style.color = "red";
+console.log(tag_h1)
+
+
+
+
+
+const boty_color = document.querySelector("body");
+
+
+// function color(param) {
+//     addEventListener("click", ()=>{
+//         param.style.backgroundColor = "aqua";
+//     })
+// }
+// color(boty_color)
+
+
+const paragraf = document.createElement('p')
+paragraf.innerHTML = '1996';
+paragraf.className = 'Kartoska';
+paragraf.innerHTML = 'dUTYfree';
+boty_color.appendChild(paragraf)
